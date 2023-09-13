4ed89b961002904241f327392ef1acf8902d2296
arnefogh <andreasfogh7@gmail.com>
2023-09-13 09:52:33 +0200

    3rf3f3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mads & Arne's DogFinder</h1>
+        <h1>Mads & Arne's DogFinder hvor du kan se billeder af hunde</h1>
       </header>
     </div>
   );
