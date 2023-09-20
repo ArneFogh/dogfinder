e7e7c6af5553e5724da4451b097306c009fa20aa
madsen10 <91535029+madsen10@users.noreply.github.com>
2023-09-20 10:28:00 +0200

    nav rykket

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import DogApp from './DogApp';
-import AddBreed from './AddBreed';
+import AddBreed from './favorites';
+import styles from './App.css';
 
 function App() {
     return (
         <Router>
-            <div>
-                {/* Navigation */}
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/add-breed">Add Breed</Link>
-                        </li>
-                    </ul>
-                </nav>
+            <div className={styles.appContainer}>
+                <header>
+                    <nav className={styles.navContainer}>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/favorites">Favorites</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
 
-                {/* Page Content */}
-                <Routes>
-                    <Route path="/" element={<DogApp />} />
-                    <Route path="/add-breed" element={<AddBreed />} />
-                </Routes>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<DogApp />} />
+                        <Route path="/add-breed" element={<AddBreed />} />
+                    </Routes>
+                </main>
             </div>
         </Router>
     );
